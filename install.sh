756fce4b63f83a8695cc0dc27588415526fe9db4
matthew.zember@gmail.com <zemberdotnet>
2020-05-23 19:58:19 +0000

    cont

--- install.sh
+++ install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 pacman -Syu
 pacman -S feh firefox firejail discord go
-mv $PWD/config $HOME/.i3/
-mv $PWD/.vimrc $HOME/
-mv $PWD/.bashrc $HOME/
-mv $PWD/.Xresources $HOME/
-xrdb $HOME/.Xresources
-
